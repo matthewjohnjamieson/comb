012f1f6ca0e6fc182332bcbb234e5dc963003ac9
EdVeras <35542690+EdVeras@users.noreply.github.com>
2018-03-15 11:48:54 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -14,8 +14,8 @@ let map;
 
 function setup() {
   pixelDensity(1); // set pix density for high dens displays
-  frameRate(30);
-  createCanvas(1440,900);
+  frameRate(60); //changed from 30
+  createCanvas(windowWidth, windowHeight); //changed added windowWidth, windowHeight insted of 
   screens[0] = new Screen(128, [(new CellGrid(width/2,height/2,30,'C')), /*(new CellGrid(width/6,height/6,30,'D'))*/ ]); //main grid
   //screens[1] = new Screen(128, [(new Cell(width/2,height/2,100,255,255,(new Chord("D", "min", "7"))))]); //playin' around screen
   screens[(screens[1] ? 1 : 0)].displayMap();
@@ -24,12 +24,17 @@ function setup() {
 }
 
 function draw() {
+  //background(0, 100, 200); may ot maynot be needed
   screens[(screens[1] ? 1 : 0)].display();
-
+  
   // DEBUGGING: display color under mouse when clicked 
   // if(mouseIsPressed){
   //   console.log(colorUnderMouse());
   // }
+}
+
+function windowResized() { // added new just for resize
+  resizeCanvas(windowWidth, windowHeight);
 }
 
 function mouseClicked(){
