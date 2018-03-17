783990f71306cb879908d3a28f222dcb21655728
matthewjohnjamieson <matthewjohnjamieson@gmail.com>
2018-03-17 18:19:09 -0400

    removed envelope distortion in synth

--- components/CombSynth.js
+++ components/CombSynth.js
@@ -1,8 +1,6 @@
 /*
 Synth object will play the notes. global and persistant so that all chords can share the synth
-currently set up with only 3 voices for simplicity
-future: add an amp envelope, this will need a gui at some point to edit it's attributes
-(another screen for this) 
+
 */
 class Synth{
   constructor(){
@@ -17,38 +15,35 @@ class Synth{
       (new p5.Env()),
       (new p5.Env())
     ];
+
+    //total amplitude can't excede 1.0, this constant divides 1 by the num of 
+    //voices in the synth, so that they don't add up > 1
+    this.MAXAMP = 1/this.voices.length;
+
+    this.voices.map(v => v.amp(0)); //init amplitude
+    this.voices.map(v => v.start());//start the oscilators
   }
 
   play(root,third,fifth){ 
 
-    //this.envelope = new p5.Env();
-    //this.envelope.play(this.voices.map(v), 0, 0.1);
-   // this.envelope.setADSR(0.001, 0.5, 0.1, 0.5) //(set attackTime, decayTime, sustainRatio, releaseTime)
-    this.envelope.map(e => e.setADSR(0.001, 0.5, 0.1, 0.5));
+    this.envelope.map(e => e.setADSR(0.01, 0.5, 0.1, 0.7));
+    this.envelope.map(e => e.setRange( (this.MAXAMP) , 0)); //highest and lowest volumes
+    
 
-   // this.envelope.setRange(1,0); //(set attackLevel, releaseLevel)
-    this.envelope.map(e => e.setRange(1, 0));
-
-    this.voices.map(v => v.start());
-
-    // this.voices[0].amp(0.5, 4);
-    // this.voices[1].amp(0.5, 4);
-    // this.voices[2].amp(0.5, 4);
-    this.voices.map(v => v.amp(0.5, 4));
+    this.voices[0].amp(this.envelope[0]);
+    this.voices[1].amp(this.envelope[1]);
+    this.voices[2].amp(this.envelope[2]);
 
     this.voices[0].freq(midiToFreq(root));
     this.voices[1].freq(midiToFreq(third));
     this.voices[2].freq(midiToFreq(fifth));
-    
-    this.envelope[0].play(this.voices[0], 0, 0.1);
-    this.envelope[1].play(this.voices[1], 0, 0.1);
-    this.envelope[2].play(this.voices[2], 0, 0.1);
-
-  
+        
+    this.envelope[0].play(this.voices[0]);
+    this.envelope[1].play(this.voices[1]);
+    this.envelope[2].play(this.voices[2]);
     }
   
-
   stop(){
-    this.voices.map(v => v.stop());
+    this.voices.map(v => v.amp(0));
   }
 }
