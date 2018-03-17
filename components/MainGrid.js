695584a574d46e96e932e130080933e50a78c6b4
vfaiola <vfaiola8173@gmail.com>
2018-03-17 19:45:29 -0400

    Displays all 12 keys repeating F# on both ends for symmetry. I could probably have implemented it in a better way but this works!

--- components/MainGrid.js
+++ components/MainGrid.js
@@ -20,14 +20,18 @@ class MainGrid{
     //this.cellSize = cellSize;
     //this.cells = this.makeCells();
 //	this.gridArray = [(new CellGrid(width/2,height/2,30,'C'))];
-	this.circleOfFifthsKeys = ['F#','B','E','A','D','G'];
+	this.circleOfFifthsKeys = ['F#','C#','G#','D#','A#','F','F#','B','E','A','D','G']; //first half is for the 'flat' keys and second half is for the others
 	this.colorWheel = ['RED','GREEN','BLUE'];
+	this.reverseColorWheel = ['RED','BLUE','GREEN'];
   }
   
   buildMainGrid(){
     var tempArray = [(new CellGrid(width/2,height/2,30,'C','RED'))];
     for(var i = 1; i <= 6; i++){
       tempArray.push(new CellGrid(width/2 + (36*3*i),height/2,30,this.circleOfFifthsKeys.pop(),this.colorWheel[i%3]));
+    }
+ 	for(var i = 1; i <= 6; i++){
+      tempArray.push(new CellGrid(width/2 - (36*3*i),height/2,30,this.circleOfFifthsKeys.pop(),this.reverseColorWheel[i%3]));
     }
     return tempArray;
   }
